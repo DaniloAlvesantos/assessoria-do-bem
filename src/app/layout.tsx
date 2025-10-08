a60d29a9eb228e82f03757b932252c8cd1bbf7a0
DaniloAlvesantos <daniloasan.itapira@gmail.com>
2025-10-08 11:06:16 -0300

    gTag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins, Montserrat } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const DEFAULT_URL = "https://assessoria-do-bem-design.netlify.app";
 const DEFAULT_DESCRIPTION = "";
@@ -75,10 +76,38 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <head />
       <body
         className={`${fontPoppins.variable} ${fontMontserrat.variable} antialiased`}
       >
+        {/* Google Tag Manager Script */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NGSGNFMG');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {children}
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NGSGNFMG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
       </body>
     </html>
   );
